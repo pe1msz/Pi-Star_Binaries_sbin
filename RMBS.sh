dd620d806d7757fc2e405c0c462c82c14ceff8f2
root <root@pi-star>
2020-03-13 05:16:40 +0100

    hotspot setting fix

--- RMBS.sh
+++ RMBS.sh
@@ -32,6 +32,7 @@
 #  sudo sed -i "/dvmegaPort=/c\\dvmegaPort=/dev/ttyS2" /etc/dstarrepeater
 
 # Here we edit the files we need, and restart
+sudo sed -i "s%.*Hardware=dvmpicasthd.*%Hardware=dvmpicast%" /etc/dstar-radio.mmdvmhost
 sudo sed -i "s%.*Hardware=dvmpicasths.*%Hardware=dvmpicast%" /etc/dstar-radio.mmdvmhost
 sudo sed -i "s%.*Port=/dev/ttyS2.*%Port=/dev/ttyAMA0%" /etc/mmdvmhost
 sudo sed -i "/dvmegaVariant=/c\\dvmegaVariant=2" /etc/dstarrepeater
